f6d446887681f5c06fe20c80f3a6a602e09e5599
matheus-chagas <matheus.fernando.chagas@outlook.com.br>
2025-05-21 00:31:41 -0300

    commit final

---
+++ src/exercicio.js
@@ -0,0 +1,14 @@
+const alunos = [
+{nome: "Matheus", nota: 11},
+	{nome: "João", nota: 8},
+	{nome: "Ana", nota: 7},
+	{nome: "Lucas", nota: 6},
+	{nome: "Luana", nota: 5},
+	{nome: "Gian", nota: 4}
+];
+
+const alunosAprovados = alunos.filter(function(aluno) {
+	return Number(aluno.nota) >= 6 && Number(aluno.nota) <= 10;
+});
+
+console.log(alunosAprovados);
